2345d98a5b73d6cd6d0aee6d04b895d80aa8d294
timatron <timatron@gmail.com>
2020-11-07 11:10:21 -0800

    use js to change the styles of post-inner

--- marionavilaros.js
+++ marionavilaros.js
@@ -1,4 +1,22 @@
+/**
+ * Utility function to add CSS in multiple passes.
+ * @param {string} styleString
+ */
+function addStyle(styleString) {
+  const style = document.createElement('style');
+  style.textContent = styleString;
+  document.head.append(style);
+}
+
+addStyle(`
+  #post-inner {
+    position: absolute;
+    top: 0;
+  }
+`);
+
+
 window.addEventListener('load', () => {
   const postInner = document.getElementById('post-inner');
-  postInner.scrollIntoView();
+  // postInner.scrollIntoView();
 });
