16c8e1c36e0091993fb5af9a84aeff28711a74b9
timatron <timatron@gmail.com>
2020-11-08 09:01:47 -0800

    test

--- marionavilaros.js
+++ marionavilaros.js
@@ -1,35 +1,37 @@
-if ( document.body.classList.contains('template-cover') ) {
-  if (document.cookie.indexOf('visited=true') == -1 || 0 < window.location.href.indexOf('?develop=') ) {
-    // set cookie
-    var thirtydays = 1000*60*60*24*30;
-    var expires = new Date((new Date()).valueOf() +  thirtydays);
-    document.cookie = "visited=true;expires=" +  expires.toUTCString();
+document.querySelector('.cover-header').addEventListener('load', function(){
+  console.log('yo');
+  $('#site-header').css('display', 'none');
+  $('.cover-header').css('display', 'none');
+  $('#post-inner').css('display', 'none');
+  $('#site-header').css('opacity', '1');
+  $('.cover-header').css('opacity', '1');
+  $('#post-inner').css('opacity', '1');
 
-    // lock screen
-    window.scrollTo(0,0);
-    document.body.classList.add('stop-scrolling');
-
-    // do intro
-    document.querySelector('.cover-header').addEventListener('load', function(){
-      console.log('yo');
-      $('#site-header').css('display', 'none');
-      $('.cover-header').css('display', 'none');
-      $('#post-inner').css('display', 'none');
-      $('#site-header').css('opacity', '1');
-      $('.cover-header').css('opacity', '1');
-      $('#post-inner').css('opacity', '1');
-
-      $('.cover-header').fadeIn(2000, function () {
-        $('#site-header').fadeIn(1000, function () {
-          $('#site-header').fadeOut(1000);
-          $('.cover-header').fadeOut(1000, function () {
-            $('#site-header').fadeIn(1000);
-            $('#post-inner').fadeIn(1000, function () {
-              $('body').removeClass('stop-scrolling');
-            });
-          });
-        });  
+  $('.cover-header').fadeIn(2000, function () {
+    $('#site-header').fadeIn(1000, function () {
+      $('#site-header').fadeOut(1000);
+      $('.cover-header').fadeOut(1000, function () {
+        $('#site-header').fadeIn(1000);
+        $('#post-inner').fadeIn(1000, function () {
+          $('body').removeClass('stop-scrolling');
+        });
       });
-    });
-  }
-}
+    });  
+  });
+});
+
+// if ( document.body.classList.contains('template-cover') ) {
+//   if (document.cookie.indexOf('visited=true') == -1 || 0 < window.location.href.indexOf('?develop=') ) {
+//     // set cookie
+//     var thirtydays = 1000*60*60*24*30;
+//     var expires = new Date((new Date()).valueOf() +  thirtydays);
+//     document.cookie = "visited=true;expires=" +  expires.toUTCString();
+
+//     // lock screen
+//     window.scrollTo(0,0);
+//     document.body.classList.add('stop-scrolling');
+
+//     // do intro
+   
+//   }
+// }
