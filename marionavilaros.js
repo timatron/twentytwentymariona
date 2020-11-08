ee8654d24adf0ca478ad0c41f55eb2eead76ea01
timatron <timatron@gmail.com>
2020-11-08 08:09:50 -0800

    simplify

--- marionavilaros.js
+++ marionavilaros.js
@@ -1,16 +1,6 @@
-/**
- * Utility function to add CSS in multiple passes.
- * @param {string} styleString
- */
-function addStyle(styleString) {
-  const style = document.createElement('style');
-  style.textContent = styleString;
-  document.head.append(style);
-}
-
 if ( document.body.classList.contains('template-cover') ) {
-
   if (document.cookie.indexOf('visited=true') == -1 || 0 < window.location.href.indexOf('?develop=') ) {
+    // set cookie
     var thirtydays = 1000*60*60*24*30;
     var expires = new Date((new Date()).valueOf() +  thirtydays);
     document.cookie = "visited=true;expires=" +  expires.toUTCString();
@@ -19,44 +9,20 @@ if ( document.body.classList.contains('template-cover') ) {
     window.scrollTo(0,0);
     document.body.classList.add('stop-scrolling');
 
+    // do intro
     jQuery(document).ready(function( $ ) {
       $('#site-header').css('display', 'none');
-      $('#site-header').css('-webkit-animation', 'none');
-      $('#site-header').css('-moz-animation', 'none');
-      $('#site-header').css('-ms-animation', 'none');
-      $('#site-header').css('-o-animation', 'none');
-      $('#site-header').css('animation', 'none');
-
-      $('#site-header').fadeIn(2000);
       $('.cover-header').fadeIn(2000, function () {
-        $('#site-header').fadeOut(1000);
-        $('.cover-header').fadeOut(1000, function () {
-          $('#site-header').fadeIn(1000);
-          $('#post-inner').fadeIn(1000, function () {
-            $('body').removeClass('stop-scrolling');
+        $('#site-header').fadeIn(1000, function () {
+          $('#site-header').fadeOut(1000);
+          $('.cover-header').fadeOut(1000, function () {
+            $('#site-header').fadeIn(1000);
+            $('#post-inner').fadeIn(1000, function () {
+              $('body').removeClass('stop-scrolling');
+            });
           });
-        });
+        });  
       });
     });
-  } else {
-    addStyle(`
-      #post-inner {
-        position: absolute;
-        top: 0;
-        z-index: 0;
-      }
-
-      .to-the-content-wrapper {
-        display: none;
-      }
-
-      .cover-header {
-        display: none;
-      }
-
-      #site-header {
-        display: block;
-      }
-    `);
   }
 }
