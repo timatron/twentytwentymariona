e744b986e5cb43f09fc9930ea0d129723f14709d
timatron <timatron@gmail.com>
2020-11-08 08:11:23 -0800

    header flash

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
 		wp_body_open();
 		?>
 
-		<header id="site-header" class="header-footer-group" role="banner">
+		<header id="site-header" class="header-footer-group" role="banner" style="display:none;">
 
 			<div class="header-inner section-inner">
 
