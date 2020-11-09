7bb3a8735ea4f419b7c303bb24e9b431f834623d
timatron <timatron@gmail.com>
2020-11-08 18:42:17 -0800

    oh boy

--- marionavilaros.js
+++ marionavilaros.js
@@ -6,6 +6,9 @@ function doIntro() {
   console.log('hellyeah');
 
   if ( document.body.classList.contains('template-cover') ) {
+
+    console.log('oh boy');
+
     // if ( document.cookie.indexOf('visited=true') == -1 || 0 < window.location.href.indexOf('?develop=') ) {
     // if ( 0 < window.location.href.indexOf('?develop=') ) {
       // set cookie
