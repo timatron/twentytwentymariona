c24d2d7ba092bffc52906ea037a29b035f461f9c
timatron <timatron@gmail.com>
2020-11-08 18:14:51 -0800

    jqery

--- marionavilaros.js
+++ marionavilaros.js
@@ -1,27 +1,29 @@
 console.log('ready');
 
 function doIntro() {
+  var $j = jQuery.noConflict();
+
   console.log('hellyeah');
-  
+
   // lock screen
   window.scrollTo(0,0);
   document.body.classList.add('stop-scrolling');
 
   // do intro
-  $('#site-header').css('display', 'none');
-  $('.cover-header').css('display', 'none');
-  $('#post-inner').css('display', 'none');
-  $('#site-header').css('opacity', '1');
-  $('.cover-header').css('opacity', '1');
-  $('#post-inner').css('opacity', '1');
+  $j('#site-header').css('display', 'none');
+  $j('.cover-header').css('display', 'none');
+  $j('#post-inner').css('display', 'none');
+  $j('#site-header').css('opacity', '1');
+  $j('.cover-header').css('opacity', '1');
+  $j('#post-inner').css('opacity', '1');
 
-  $('.cover-header').fadeIn(2000, function () {
-    $('#site-header').fadeIn(1000, function () {
-      $('#site-header').fadeOut(1000);
-      $('.cover-header').fadeOut(1000, function () {
-        $('#site-header').fadeIn(1000);
-        $('#post-inner').fadeIn(1000, function () {
-          $('body').removeClass('stop-scrolling');
+  $j('.cover-header').fadeIn(2000, function () {
+    $j('#site-header').fadeIn(1000, function () {
+      $j('#site-header').fadeOut(1000);
+      $j('.cover-header').fadeOut(1000, function () {
+        $j('#site-header').fadeIn(1000);
+        $j('#post-inner').fadeIn(1000, function () {
+          $j('body').removeClass('stop-scrolling');
         });
       });
     });  
