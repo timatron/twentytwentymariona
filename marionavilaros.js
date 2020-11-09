6aab395ac8299a215e87af6207c1e60bf0d46866
timatron <timatron@gmail.com>
2020-11-08 18:13:05 -0800

    lets get lucky

--- marionavilaros.js
+++ marionavilaros.js
@@ -2,28 +2,33 @@ console.log('ready');
 
 function doIntro() {
   console.log('hellyeah');
+  
+  // lock screen
+  window.scrollTo(0,0);
+  document.body.classList.add('stop-scrolling');
+
+  // do intro
+  $('#site-header').css('display', 'none');
+  $('.cover-header').css('display', 'none');
+  $('#post-inner').css('display', 'none');
+  $('#site-header').css('opacity', '1');
+  $('.cover-header').css('opacity', '1');
+  $('#post-inner').css('opacity', '1');
+
+  $('.cover-header').fadeIn(2000, function () {
+    $('#site-header').fadeIn(1000, function () {
+      $('#site-header').fadeOut(1000);
+      $('.cover-header').fadeOut(1000, function () {
+        $('#site-header').fadeIn(1000);
+        $('#post-inner').fadeIn(1000, function () {
+          $('body').removeClass('stop-scrolling');
+        });
+      });
+    });  
+  });
 
 };
 
-  // $('#site-header').css('display', 'none');
-  // $('.cover-header').css('display', 'none');
-  // $('#post-inner').css('display', 'none');
-  // $('#site-header').css('opacity', '1');
-  // $('.cover-header').css('opacity', '1');
-  // $('#post-inner').css('opacity', '1');
-
-  // $('.cover-header').fadeIn(2000, function () {
-  //   $('#site-header').fadeIn(1000, function () {
-  //     $('#site-header').fadeOut(1000);
-  //     $('.cover-header').fadeOut(1000, function () {
-  //       $('#site-header').fadeIn(1000);
-  //       $('#post-inner').fadeIn(1000, function () {
-  //         $('body').removeClass('stop-scrolling');
-  //       });
-  //     });
-  //   });  
-  // });
-// });
 
 // if ( document.body.classList.contains('template-cover') ) {
 //   if (document.cookie.indexOf('visited=true') == -1 || 0 < window.location.href.indexOf('?develop=') ) {
@@ -32,11 +37,7 @@ function doIntro() {
 //     var expires = new Date((new Date()).valueOf() +  thirtydays);
 //     document.cookie = "visited=true;expires=" +  expires.toUTCString();
 
-//     // lock screen
-//     window.scrollTo(0,0);
-//     document.body.classList.add('stop-scrolling');
 
-//     // do intro
    
 //   }
 // }
