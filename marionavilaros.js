9deb0805cb444b74d6368700c1316bf9959b9ca4
timatron <timatron@gmail.com>
2020-11-10 11:12:09 -0800

    wait a second longer

--- marionavilaros.js
+++ marionavilaros.js
@@ -22,13 +22,16 @@ function doIntro() {
       jQuery('#site-header').css('display', 'none');
       jQuery('#post-inner').css('display', 'none');
       jQuery('#coverall').css('display', 'none');
+      jQuery('.cover-branding').css('display', 'none');
       
-      jQuery('#coverall').delay(1000).fadeIn(2000, function () {
-        jQuery('#coverall').delay(1000).fadeOut(1000, function () {
-          jQuery('#post-inner').fadeIn(1000, function () {
-            jQuery('.cover-branding').fadeOut(1000);
-            jQuery('#site-header').fadeIn(1000);
-            jQuery('body').removeClass('stop-scrolling');
+      jQuery('.cover-branding').fadeIn(1000, function () {
+        jQuery('#coverall').delay(2000).fadeIn(2000, function () {
+          jQuery('#coverall').delay(1000).fadeOut(1000, function () {
+            jQuery('#post-inner').fadeIn(1000, function () {
+              jQuery('.cover-branding').fadeOut(1000);
+              jQuery('#site-header').fadeIn(1000);
+              jQuery('body').removeClass('stop-scrolling');
+            });
           });
         });
       });
