3499765ac35b6cf8d0a0fcfa4d4d94416099d7d8
timatron <timatron@gmail.com>
2020-11-10 17:35:51 -0800

    turn on cookie

--- marionavilaros.js
+++ marionavilaros.js
@@ -1,13 +1,10 @@
 function doIntro() {
-
   if ( document.body.classList.contains('template-cover') ) {
-
-    // if ( document.cookie.indexOf('visited=true') == -1 || 0 < window.location.href.indexOf('?develop=') ) {
-    // if ( 0 < window.location.href.indexOf('?develop=') ) {
-      // set cookie
-      // var thirtydays = 1000*60*60*24*30;
-      // var expires = new Date((new Date()).valueOf() +  thirtydays);
-      // document.cookie = "visited=true;expires=" +  expires.toUTCString();
+    if ( document.cookie.indexOf('visited=true') == -1 || 0 < window.location.href.indexOf('?develop=') ) {
+      set cookie
+      var thirtydays = 1000*60*60*24*30;
+      var expires = new Date((new Date()).valueOf() +  thirtydays);
+      document.cookie = "visited=true;expires=" +  expires.toUTCString();
      
       // lock screen
       window.scrollTo(0,0);
@@ -30,8 +27,6 @@ function doIntro() {
           });
         });
       });
-     
-   
-    // }
+    }
   }
 };
